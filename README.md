a2d2d4a7a441d30ff72e8af7069067b53e609ccb
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-07-29 14:10:53 -0400

    added links to skills & built readme file

--- README.md
+++ README.md
@@ -1,13 +1,41 @@
-# Alan's Portfolio
+# React-Portfolio
 
-Welcome! Thanks for checking out my portfolio. I built this site using React and 
+# Table of Contents
 
-## Tech Used
+- [Description](#description)
+- [Installation](#installation)
+- [Contributing](#contributing)
+- [License](#license)
+- [Issues](#issues)
+- [Questions](#questions)
 
-HTML, CSS, and a very small amount of Javascript.
+## Description:
+![License](https://img.shields.io/badge/License-ISC-blue.svg "License Badge")
 
-### Issues
+My portfolio was made with React.js and is constantly evolving as my projects and abilities increase. Thanks for checking it out.  I hope you like it and be sure to check back in the future for new updates.
 
-SVG's were used for the Github and Linkedin icons which created a slight contrast issue as flagged by Lighthouse.  
+![React-Portfolio](src/assets/images/portfolioRM.png)
 
-I could not resolve this so feel free to submit working solutions!
+[Link to Deployed Site]()
+
+## Installation:
+After cloning the repo, run `npm install` to install dependencies. Then run `npm start` to run the application locally.
+
+## Contributing:
+Please submit a pull request.  For large changes please email me first. All feedback and input on making my portfolio are welcomed.
+
+## License:
+For more information about the License, click on the link below.
+
+## Issues:
+N/A
+
+- [License](https://opensource.org/licenses/ISC)
+
+##  Questions:
+For questions about the app you can go to my 
+Github page at the following link:
+
+- [Github Profile](https://github.com/abalcs)
+
+For additional questions please reach out to me via email at: abalcom23@gmail.com.
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,17 +18,17 @@ function Skills() {
       <h2>Technical Skills</h2>
       <div className="skill-list">
         <ul className="list">
-          <li><img src={html} className="skill-icon" alt="html"/></li>
-          <li><img src={js} className="skill-icon js" alt="js"/></li>
-          <li><img src={bootstrap} className="skill-icon" alt="bootstrap"/></li>
-          <li><img src={css} className="skill-icon css" alt="css"/></li>
-          <li><img src={git} className="skill-icon" alt="git"/></li>
-          <li><img src={jquery} className="skill-icon" alt="jquery"/></li>
-          <li><img src={mongodb} className="skill-icon" alt="mongodb"/></li>
-          <li><img src={node} className="skill-icon" alt="node"/></li>
-          <li><img src={react} className="skill-icon" alt="react"/></li>
-          <li><img src={sql} className="skill-icon sql" alt="sql"/></li>
-          <li><img src={webpack} className="skill-icon" alt="webpack"/></li>
+          <li><a href="https://en.wikipedia.org/wiki/HTML" target="_blank" rel="noreferrer"><img src={html} className="skill-icon" alt="html"/></a></li>
+          <li><a href="https://www.javascript.com" target="_blank" rel="noreferrer"><img src={js} className="skill-icon js" alt="js"/></a></li>
+          <li><a href="https://getbootstrap.com/" target="_blank" rel="noreferrer"><img src={bootstrap} className="skill-icon" alt="bootstrap"/></a></li>
+          <li><a href="https://en.wikipedia.org/wiki/CSS" target="_blank" rel="noreferrer"><img src={css} className="skill-icon css" alt="css"/></a></li>
+          <li><a href="https://git-scm.com/" target="_blank" rel="noreferrer"><img src={git} className="skill-icon" alt="git"/></a></li>
+          <li><a href="https://jquery.com/" target="_blank" rel="noreferrer"><img src={jquery} className="skill-icon" alt="jquery"/></a></li>
+          <li><a href="https://www.mongodb.com/cloud/atlas/lp/try2?utm_source=google&utm_campaign=gs_americas_united_states_search_core_brand_atlas_desktop&utm_term=mongodb&utm_medium=cpc_paid_search&utm_ad=e&utm_ad_campaign_id=12212624338&gclid=CjwKCAjwo4mIBhBsEiwAKgzXOHDccwL_lR8Q00vmHOY3fEVJXLKCQigvw8Wax0IG6oBvCNB3g_QD2BoC9o0QAvD_BwE" target="_blank" rel="noreferrer"><img src={mongodb} className="skill-icon" alt="mongodb"/></a></li>
+          <li><a href="https://nodejs.org/en/" target="_blank" rel="noreferrer"><img src={node} className="skill-icon" alt="node"/></a></li>
+          <li><a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img src={react} className="skill-icon" alt="react"/></a></li>
+          <li><a href="https://en.wikipedia.org/wiki/SQL" target="_blank" rel="noreferrer"><img src={sql} className="skill-icon sql" alt="sql"/></a></li>
+          <li><a href="https://webpack.js.org/" target="_blank" rel="noreferrer"><img src={webpack} className="skill-icon" alt="webpack"/></a></li>
         </ul>
       </div>
       </section>
