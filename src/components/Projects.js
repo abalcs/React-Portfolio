4c9ed3cfc181372b24fab4e9dde3a0b2cae8fca5
Alan Balcom <abalcom23@gmail.com>
2022-11-22 13:22:03 -0500

    added 3d carousel for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,8 @@ import IconButton from '@mui/material/IconButton';
 import Github from '@mui/icons-material/GitHub';
 import WebIcon from '@mui/icons-material/Web';
 
+import {Carousel} from '3d-react-carousal';
+
 import listManager from '../assets/images/ListManager.jpg';
 import ecfd from '../assets/images/ecfd.jpg';
 import spacex from '../assets/images/spacex.jpg';
@@ -16,189 +18,194 @@ import marvel from '../assets/images/marvel.png';
 
 function Projects() {
 
+  let slides = [
+    <Card 
+      sx={{ maxWidth: 345, height: 500 }}
+      style={{ borderRadius: '10px', border: '1px solid black' }}
+      >
+      <CardHeader
+        title="LIST MANAGER"
+        style={{backgroundColor: "black", color: 'white'}}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={listManager}
+        alt="List Manager"
+        style={{ marginTop: '10px' }}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          I built this CRUD list manager to have an easy way for my family to keep track of the items we need.
+        </Typography>
+        <br></br>
+        <Typography variant="body2" color="text.secondary">
+          Tech Used: <em>React, JavaScript, MongoDB, NodeJS, Express</em>
+        </Typography>
+      </CardContent>
+      <CardActions 
+      disableSpacing
+      style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <IconButton 
+        aria-label="add to favorites"
+        href='https://github.com/abalcs/Grocery_List'
+        target='_blank'
+        >
+          <Github />
+        </IconButton>
+        <IconButton 
+        aria-label="share"
+        href='https://family-grocery-app.herokuapp.com/'
+        target='_blank'>
+          <WebIcon />
+        </IconButton>
+      </CardActions>
+    </Card>,
+
+    <Card 
+      sx={{ maxWidth: 345, height: 500 }}
+      style={{ borderRadius: '10px', border: '1px solid black' }}
+      >
+      <CardHeader
+        title="ERIE COUNTY FIRE DEPT"
+        style={{backgroundColor: "black", color: 'white'}}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={ecfd}
+        alt="ECFD Site Template"
+        style={{ marginTop: '10px' }}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Erie Country, NY Fire District needed a website. This is the template I created and provided to them.
+        </Typography>
+        <br></br>
+        <Typography variant="body2" color="text.secondary">
+          Tech Used: <em>React, JavaScript, CSS, Bootstrap, NodeJS, Express</em>
+        </Typography>
+      </CardContent>
+      <CardActions 
+      disableSpacing
+      style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <IconButton 
+        aria-label="add to favorites"
+        href='https://github.com/abalcs/ecfd'
+        target='_blank'
+        >
+          <Github />
+        </IconButton>
+        <IconButton 
+        aria-label="share"
+        href='http://ecfd.herokuapp.com/'
+        target='_blank'>
+          <WebIcon />
+        </IconButton>
+      </CardActions>
+    </Card>,
+
+    <Card 
+      sx={{ maxWidth: 345, height: 500 }}
+      style={{ borderRadius: '10px', border: '1px solid black' }}
+      >
+      <CardHeader
+        title="SpaceX Launches"
+        style={{backgroundColor: "black", color: 'white'}}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={spacex}
+        alt="List Manager"
+        style={{ marginTop: '10px' }}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          Tracks and displays information on SpaceX launches. Uses React for front end, Express, GraphQL, and the SpaceX API.
+        </Typography>
+        <br></br>
+        <Typography variant="body2" color="text.secondary">
+          Tech Used: <em>React, JavaScript, NodeJS, Express, GraphQL, SpaceX API</em>
+        </Typography>
+      </CardContent>
+      <CardActions 
+      disableSpacing
+      style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <IconButton 
+        aria-label="add to favorites"
+        href='https://github.com/abalcs/SpaceX_Launches'
+        target='_blank'
+        >
+          <Github />
+        </IconButton>
+        <IconButton 
+        aria-label="share"
+        href='https://github.com/abalcs/SpaceX_Launches'
+        target='_blank'>
+          <WebIcon />
+        </IconButton>
+      </CardActions>
+    </Card>,
+
+    <Card 
+      sx={{ maxWidth: 345, height: 500 }}
+      style={{ borderRadius: '10px', border: '1px solid black' }}
+      >
+      <CardHeader
+        title="MARVEL ARCHIVE"
+        style={{backgroundColor: "black", color: 'white'}}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={marvel}
+        alt="List Manager"
+        style={{ marginTop: '10px' }}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          .
+        </Typography>
+        <br></br>
+        <Typography variant="body2" color="text.secondary">
+          Tech Used: <em>JavaScript, Marvel API, CSS, & HTML</em>
+        </Typography>
+      </CardContent>
+      <CardActions 
+      disableSpacing
+      style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <IconButton 
+        aria-label="add to favorites"
+        href='https://github.com/abalcs/Marvel-Archive'
+        target='_blank'
+        >
+          <Github />
+        </IconButton>
+        <IconButton 
+        aria-label="share"
+        href='https://abalcs.github.io/Marvel-Archive/'
+        target='_blank'>
+          <WebIcon />
+        </IconButton>
+      </CardActions>
+    </Card>,
+  ]
+
+  const callback = function(index){
+    console.log("callback",index);
+}
 
   return (
-    <section class="sub-section-alternative" id="projects">
-      <h2>PROJECTS</h2>
-      <br></br>
-        <div class="project-container">
-          <Card 
-            sx={{ maxWidth: 345 }}
-            style={{ borderRadius: '10px', border: '1px solid black' }}
-            >
-            <CardHeader
-              title="LIST MANAGER"
-              style={{backgroundColor: "black", color: 'white'}}
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image={listManager}
-              alt="List Manager"
-              style={{ marginTop: '10px' }}
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                I built this CRUD list manager to have an easy way for my family to keep track of the items we need.
-              </Typography>
-              <br></br>
-              <Typography variant="body2" color="text.secondary">
-                Tech Used: <em>React, JavaScript, MongoDB, NodeJS, Express</em>
-              </Typography>
-            </CardContent>
-            <CardActions 
-            disableSpacing
-            style={{ display: 'flex', justifyContent: 'center' }}
-            >
-              <IconButton 
-              aria-label="add to favorites"
-              href='https://github.com/abalcs/Grocery_List'
-              target='_blank'
-              >
-                <Github />
-              </IconButton>
-              <IconButton 
-              aria-label="share"
-              href='https://family-grocery-app.herokuapp.com/'
-              target='_blank'>
-                <WebIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
-
-          <Card 
-            sx={{ maxWidth: 345 }}
-            style={{ borderRadius: '10px', border: '1px solid black' }}
-            >
-            <CardHeader
-              title="ERIE COUNTY FIRE DEPT"
-              style={{backgroundColor: "black", color: 'white'}}
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image={ecfd}
-              alt="ECFD Site Template"
-              style={{ marginTop: '10px' }}
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                Erie Country, NY Fire District needed a website. This is the template I created and provided to them.
-              </Typography>
-              <br></br>
-              <Typography variant="body2" color="text.secondary">
-                Tech Used: <em>React, JavaScript, CSS, Bootstrap, NodeJS, Express</em>
-              </Typography>
-            </CardContent>
-            <CardActions 
-            disableSpacing
-            style={{ display: 'flex', justifyContent: 'center' }}
-            >
-              <IconButton 
-              aria-label="add to favorites"
-              href='https://github.com/abalcs/ecfd'
-              target='_blank'
-              >
-                <Github />
-              </IconButton>
-              <IconButton 
-              aria-label="share"
-              href='http://ecfd.herokuapp.com/'
-              target='_blank'>
-                <WebIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
-
-          <Card 
-            sx={{ maxWidth: 345 }}
-            style={{ borderRadius: '10px', border: '1px solid black' }}
-            >
-            <CardHeader
-              title="SpaceX Launches"
-              style={{backgroundColor: "black", color: 'white'}}
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image={spacex}
-              alt="List Manager"
-              style={{ marginTop: '10px' }}
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                Tracks and displays information on SpaceX launches. Uses React for front end, Express, GraphQL, and the SpaceX API.
-              </Typography>
-              <br></br>
-              <Typography variant="body2" color="text.secondary">
-                Tech Used: <em>React, JavaScript, NodeJS, Express, GraphQL, SpaceX API</em>
-              </Typography>
-            </CardContent>
-            <CardActions 
-            disableSpacing
-            style={{ display: 'flex', justifyContent: 'center' }}
-            >
-              <IconButton 
-              aria-label="add to favorites"
-              href='https://github.com/abalcs/SpaceX_Launches'
-              target='_blank'
-              >
-                <Github />
-              </IconButton>
-              <IconButton 
-              aria-label="share"
-              href='https://github.com/abalcs/SpaceX_Launches'
-              target='_blank'>
-                <WebIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
-
-          <Card 
-            sx={{ maxWidth: 345 }}
-            style={{ borderRadius: '10px', border: '1px solid black' }}
-            >
-            <CardHeader
-              title="MARVEL ARCHIVE"
-              style={{backgroundColor: "black", color: 'white'}}
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image={marvel}
-              alt="List Manager"
-              style={{ marginTop: '10px' }}
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                .
-              </Typography>
-              <br></br>
-              <Typography variant="body2" color="text.secondary">
-                Tech Used: <em>JavaScript, Marvel API, CSS, & HTML</em>
-              </Typography>
-            </CardContent>
-            <CardActions 
-            disableSpacing
-            style={{ display: 'flex', justifyContent: 'center' }}
-            >
-              <IconButton 
-              aria-label="add to favorites"
-              href='https://github.com/abalcs/Marvel-Archive'
-              target='_blank'
-              >
-                <Github />
-              </IconButton>
-              <IconButton 
-              aria-label="share"
-              href='https://abalcs.github.io/Marvel-Archive/'
-              target='_blank'>
-                <WebIcon />
-              </IconButton>
-            </CardActions>
-          </Card>
-      </div>
-    </section>
+    <>
+    <h2 style={{ textAlign: 'center'}}>PROJECTS</h2>
+    <br></br>
+    <Carousel slides={slides} autoplay={true} interval={5000} onSlideChange={callback}/>
+    </>
   );
 }
 
